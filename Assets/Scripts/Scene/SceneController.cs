d0b9c7a6ec9cd73382d1b03d0607122104716932
Leon Liu <liuleon360@gmail.com>
2024-02-14 14:30:29 -0800

    add Main Menu base and Scene Controller

---
+++ Assets/Scripts/Scene/SceneController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public static SceneController Instance { get; private set; }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    void Awake()
+    {
+        // Singleton pattern, only one instance of this class should exist
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
