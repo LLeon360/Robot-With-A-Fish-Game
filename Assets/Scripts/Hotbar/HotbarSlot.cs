c2ed3d031126ff7dca137ac8ead22f5e3a9f5d2b
Leon Liu <liuleon360@gmail.com>
2024-02-29 00:13:45 -0800

    Add cost and cooldown overlays

--- Assets/Scripts/Hotbar/HotbarSlot.cs
+++ Assets/Scripts/Hotbar/HotbarSlot.cs
@@ -2,33 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 /**
 * HotbarSlot class
 * Stores the current HotbarElement and renders the information into this slot
 */
 public class HotbarSlot : MonoBehaviour
 {
+    [SerializeField]
     private HotbarElement hotbarElement;
     [SerializeField]
     private GameObject slotBackground;
     [SerializeField]
     private GameObject slotIcon;
+    [SerializeField]
+    private GameObject cooldownOverlay;
+    [SerializeField]
+    private TextMeshProUGUI costOverlay;
     // Start is called before the first frame update
     void Start()
     {   
-        hotbarElement = null;
         GetSlotReferences();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(this.hotbarElement == null);
+        if(hotbarElement != null)
+        {
+            if(hotbarElement.nextUsableTime <= Time.time)
+            {
+                cooldownOverlay.SetActive(false);
+            }
+            else
+            {
+                cooldownOverlay.SetActive(true);
+                UpdateCooldownOverlay();
+            }
+        }
+    }
+
+    void UpdateCooldownOverlay()
+    {
+        float cooldownTimeRemaining = hotbarElement.nextUsableTime - Time.time;
+        cooldownOverlay.GetComponent<Image>().fillAmount = cooldownTimeRemaining / hotbarElement.hotbarElementObject.cooldown;
+        cooldownOverlay.GetComponent<Image>().color = Color.Lerp(new Color(1,1f,1f,0.7f), new Color(1,0.57f,0.57f,1f), cooldownTimeRemaining / hotbarElement.hotbarElementObject.cooldown);
     }
     
     void GetSlotReferences() {
         slotBackground = transform.Find("Slot Background").gameObject;
         slotIcon = transform.Find("Slot Icon").gameObject;
+        cooldownOverlay = transform.Find("Cooldown Overlay").gameObject;
+        costOverlay = transform.Find("Cost Overlay").GetComponent<TextMeshProUGUI>();
     }
 
     public void SetHotbarElement(HotbarElement hotbarElement)
@@ -39,6 +65,7 @@ public class HotbarSlot : MonoBehaviour
         }
 
         this.hotbarElement = hotbarElement;
+        Debug.Log(this.hotbarElement);
         slotIcon.GetComponent<Image>().sprite = hotbarElement.hotbarElementObject.icon;
         
         //if is buliding set background to green, if unit set to purple
@@ -55,5 +82,8 @@ public class HotbarSlot : MonoBehaviour
                 Debug.LogError("Unknown slot type: " + hotbarElement.hotbarElementObject.slotType);
                 break;
         }
+
+        cooldownOverlay.SetActive(false);
+        costOverlay.text = "$"+hotbarElement.hotbarElementObject.cost.ToString();
     }
 }
