223ed5ef46ca38455d9c6cfb2b2b26327a0cecf7
Leon Liu <liuleon360@gmail.com>
2024-03-05 14:11:58 -0800

    Import sprites

---
+++ Assets/Scripts/Unit/Abilities/Spawner.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+[RequireComponent(typeof(UnitInfoScript))]
+public class Spawner : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> unitPrefabs;
+
+    private UnitInfoScript unitInfo;
+    void Start()
+    {
+        unitInfo = gameObject.GetComponent<UnitInfoScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Deploy(int lane, int tile, int unitIndex) {
+        if(unitIndex < 0 || unitIndex >= unitPrefabs.Count) {
+            Debug.LogError("Invalid unit index");
+            return;
+        }
+        GameObject unitPrefab = unitPrefabs[unitIndex];
+        LaneManager.Instance.Deploy(lane, tile, unitPrefab, unitInfo.player, unitInfo.type=="Building");
+    }
+}
