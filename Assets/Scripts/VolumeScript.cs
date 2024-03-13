b5e68f6a4ca478c61aec18e3ac5058a49ac92525
Leon Liu <liuleon360@gmail.com>
2024-03-13 16:11:27 -0700

    Update VolumeScript.cs

--- Assets/Scripts/VolumeScript.cs
+++ Assets/Scripts/VolumeScript.cs
@@ -2,16 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio; // Required for AudioMixer
-using UnityEngine.UI; // Required for Scrollbar
+using UnityEngine.UI; // Required for Slider
 
 public class VolumeScript : MonoBehaviour
 {
     public AudioMixer audioMixer; // Reference to the AudioMixer
-    public Slider slider; // Reference to the Scrollbar
+    public Slider slider; // Reference to the Slider
 
     void Start() 
     {
-        //grab scrollbar
+        //grab slider
         slider = GetComponent<Slider>();
     }
 
@@ -21,7 +21,7 @@ public class VolumeScript : MonoBehaviour
         float minVolume = -80; // Minimum volume in dB
         float maxVolume = 0; // Maximum volume in dB
 
-        // Map the scrollbar value from [0, 1] to [minVolume, maxVolume]
+        // Map the slider value from [0, 1] to [minVolume, maxVolume]
         float volume = slider.value * (maxVolume - minVolume) + minVolume;
 
         // Set the volume of the AudioMixer
